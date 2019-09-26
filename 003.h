7e75eb33b3b2ca05be6a20fdf0937418cc037735
leizton <leizton@126.com>
2019-09-27 00:25:56 +0800

    最长不重复子串

---
+++ 003.h
@@ -0,0 +1,50 @@
+#include "basic.h"
+
+/*
+
+给定一个字符串，找出其中不含有重复字符的最长子串的长度
+
+*/
+
+int lengthOfLongestSubstring(string&& s) {
+  if (s.empty()) return 0;
+
+  int flag[256];
+  for (int i = 0; i < 256; i++) {
+    flag[i] = -1;
+  }
+  const int slen = (int)s.length();
+
+  int max_len = 1, start = 0, pos;
+  flag[s[start]] = start;
+  for (int i = 1; i < slen; i++) {
+    pos = flag[s[i]];
+    flag[s[i]] = i;
+
+    if (pos < start) {
+      continue;
+    }
+
+    if (i-start > max_len) {
+      max_len = i-start;
+    }
+    start = pos+1;
+  }
+  if (slen-start > max_len) {
+    max_len = slen-start;
+  }
+  return max_len;
+}
+
+void test() {
+  int ret;
+
+  ret = lengthOfLongestSubstring("bbbbb");  // 1
+  printer.print(ret);
+
+  ret = lengthOfLongestSubstring("bbabb");  // 2
+  printer.print(ret);
+
+  ret = lengthOfLongestSubstring("pwwkew");  // 3
+  printer.print(ret);
+}
