1d902ea1a6f0e2c7fb6dbc8d1ed8a4aade69e446
leizton <leizton@126.com>
2020-02-21 16:48:46 +0800

    027

---
+++ array/quick_sort_part/027.h
@@ -0,0 +1,41 @@
+#include "basic.h"
+
+/*
+
+# 移除元素
+给定一个数组nums和一个值val, 你需要原地移除所有数值等于val的元素, 并返回移除后数组的新长度
+空间复杂度O(1)
+元素的顺序可以改变. 你不需要考虑数组中超出新长度后面的元素
+
+# ex
+给定 nums = [3,1,2,3], val = 3
+  返回2, nums前两个元素是[1, 2], 顺序随意, 后面的元素随意
+
+#
+类似快排分partition的做法
+(==val)的是一个partition, (!=val)的是另一个partition
+
+*/
+
+int removeElement(vector<int>& nums, int val) {
+  int p = 0, last = nums.size(), t;
+  while (p < last) {
+    if (nums[p] == val) {
+      for (last--; last > p && nums[last] == val; last--);
+      if (last > p) {
+        t = nums[p];
+        nums[p] = nums[last];
+        nums[last] = t;
+      }
+    }
+    p++;
+  }
+  nums.resize(last);
+  return last;
+}
+
+void test() {
+  vector<int> v1 = {3,1,2,3};
+  Eq(removeElement(v1, 3), 2);
+  print(v1);
+}
--- basic.h
+++ basic.h
@@ -1,19 +1,34 @@
 #pragma once
 
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
 #include <exception>
 #include <typeinfo>
 
-#include <iostream>
-#include <fstream>
 #include <memory>
 #include <string>
 #include <sstream>
-
 #include <algorithm>
 #include <vector>
 #include <list>
+#include <queue>
+#include <set>
+#include <unordered_set>
 #include <map>
 #include <unordered_map>
+
+#include <type_traits>
+#include <typeinfo>
+#include <ctime>
+
+#include <chrono>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <future>
 
 #include "pprint.hpp"
 
@@ -21,27 +36,29 @@ using namespace std;
 
 pprint::PrettyPrinter printer(std::cout);
 
-#define print(...) printer.print(__VA_ARGS__)
+#define pprint(...) printer.print(__VA_ARGS__)
 
 #define COUT(expr) std::cout << std::boolalpha << (expr) << std::endl;
 
-#define FMTSTR_SIZE 1024
-#define FMTSTR\
-  char fmtstrtmp[FMTSTR_SIZE]; {va_list ap; va_start(ap, fmt);\
-  vsnprintf(fmtstrtmp, FMTSTR_SIZE, fmt, ap); va_end(ap);}
-
 void fmtprint(const char* level, const char* file, const int line,
               const char* func, const char* fmt, ...) {
-  FMTSTR;
+  const int bufsize = 1024;
+  char fmtstrtmp[bufsize];
+  {
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(fmtstrtmp, bufsize, fmt, ap);
+    va_end(ap);
+  }
+
   std::ostringstream ss;
-  // ss << "[" << file << ":" << line << " " << func << "] ";
-  ss << level << " " << line << "] ";
+  ss << level << " " << line << "]  ";
   ss << fmtstrtmp;
   std::cout << ss.str() << std::endl;
 }
 
 #define PRINT_INFO(fmt, args...)\
-  fmtprint("√", __FILE__, __LINE__, __FUNCTION__, fmt, ##args);
+  // fmtprint("√", __FILE__, __LINE__, __FUNCTION__, fmt, ##args);
 
 #define PRINT_ERROR(fmt, args...)\
   fmtprint("×", __FILE__, __LINE__, __FUNCTION__, fmt, ##args);
@@ -49,3 +66,18 @@ void fmtprint(const char* level, const char* file, const int line,
 #define Eq(actual, expect)\
   if ((actual) == (expect)) { PRINT_INFO("Eq: %s == %s", #actual, #expect); }\
   else { PRINT_ERROR("Eq: %s != %s", #actual, #expect); }
+
+
+template<class T>
+void print(std::vector<T> v) {
+  std::ostringstream out;
+  out << "[";
+  bool first = true;
+  for (T& e : v) {
+    if (!first) out << ",";
+    first = false;
+    out << e;
+  }
+  out << "]\n";
+  std::cout << out.str();
+}
