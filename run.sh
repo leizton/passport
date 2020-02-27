5f279bff50d13bc8bb58b7694dc1cfa1ee8f62ac
leizton <leizton@126.com>
2020-02-27 17:12:43 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 rm -f a.out
 
+CPP_FLAGS='g++ -std=c++17'
+
+function init_include_flags(){
+  INCLUDE_FLAGS="$INCLUDE_FLAGS -I$1"
+  for e in `ls $1`; do  
+    t=$1"/"$e
+    if [ -d $t ]; then
+      init_include_flags $t
+    fi  
+  done
+}
+INCLUDE_FLAGS=''
+init_include_flags .
+
+CPP="$CPP_FLAGS $INCLUDE_FLAGS"
+
 if [ $# -gt 0 ]; then
-  g++ -std=c++17 main.cc -DSOLU_HEADER="\"$1.h\""
+  $CPP main.cc -DSOLU_HEADER="\"$1.h\""
 else
-  g++ -std=c++17 main.cc
+  $CPP main.cc
 fi
 
 if [ -f a.out ]; then
--- string/searching/028.h
+++ string/searching/028.h
@@ -6,16 +6,19 @@
 
 查找子串位置
 
-sunday算法
-
 */
 
-int strStr(string str, string target) {
-  if (target.empty()) return 0;
-  if (str.length() < target.length()) return -1;
-  const char* a = target.c_str();
+struct Result {
+  int idx;
+  int cmp_cnt;
+};
+
+Result sunday(string str, string pattern) {
+  if (pattern.empty()) return {0, 0};
+  if (str.length() < pattern.length()) return {-1, 0};
+  const char* a = pattern.c_str();
   const char* b = str.c_str();
-  const int n1 = (int)target.length();
+  const int n1 = (int)pattern.length();
   const int n2 = (int)str.length();
 
   // 记录a中每个字符最后一次出现的位置
@@ -28,16 +31,26 @@ int strStr(string str, string target) {
   }
 
   const int limit = n2 - n1;
-  int k;
   const char* b1;
+  int k, cmp_cnt=0;
   for (int p = 0; p <= limit; ) {
     b1 = b + p;
-    for (k = 0; k<n1 && a[k]==b1[k]; k++);  // 比较(k+1)次
-    if (k == n1) return p;
-    if (p == limit) return -1;
+    for (k = 0; k<n1 && a[k]==b1[k]; k++);
+    cmp_cnt += k+1;
+    if (k == n1) return {p, cmp_cnt};
+    if (p == limit) return {-1, cmp_cnt};
     p += n1 - last_idx[ b1[n1] + 128 ];
   }
-  return -1;
+  return {-1, cmp_cnt};
+}
+
+int strStr(string str, string pattern) {
+  cout << "\n>> " << str.length() << " " << pattern.length() << endl;
+
+  Result sdy_ret = sunday(str, pattern);
+  cout << "sunday: " << sdy_ret.cmp_cnt << endl;
+
+  return sdy_ret.idx;
 }
 
 void test() {
@@ -47,7 +60,5 @@ void test() {
   ASSERT_EQ(2, strStr("abc123", "c1"));
   ASSERT_EQ(-1, strStr("abc123", "a1"));
   ASSERT_EQ(1, strStr("aabaabbbaabbbbabaaab", "abaa"));
-
-  // m=20, n=10, cmp_n=61
   ASSERT_EQ(10, strStr("bbbbbbbbbbbbbbbbbbb1", "bbbbbbbbb1"));
 }
